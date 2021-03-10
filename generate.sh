1066861c0c4d926ce17ffc58d30bede7f07c1f8d
Bilel <b.guetarni@laposte.net>
2021-03-10 13:53:35 +0100

    update

--- generate.sh
+++ generate.sh
@@ -1,30 +1,19 @@
 #!/bin/bash
-if [ "$#" -eq 0 ]
-then
-	echo "Enter number of images to consider"
-	read nb
-else
-	nb="$1"
-fi
-if ! [[ "$nb" =~ ^[0-9]+$ ]]
-then
-	echo "Input passed not a number"
-else
-	echo "begin panorama cration" > log.txt
-	for sample in $(ls data)
-	do
-		if [ -e data/ground_truth/"$sample"/out.jpg ]
+for sample in $(ls data/unblurred/)
+do
+	if [ -e /data/panorama_15/inputs/"$sample" ]
+	then
+		echo "$sample" skipped
+	else
+		echo "$sample"
+		ls -d data/unblurred/"$sample"/* | xargs ./image-stitching
+		if [ -e "out.jpg" ]
 		then
-			echo "$sample skipped"
+			mkdir /data/panorama_15/inputs/"$sample"
+			cp data/blurred/"$sample"/* /data/panorama_15/inputs/"$sample"/
+			mv out.jpg /data/panorama_15/outputs/"$sample".jpg
 		else
-			echo "$sample"
-			ls -d data/unblurred/"$sample"/* | head -n "$nb" | xargs ./image-stitching
-			if [ -e "out.jpg" ]
-			then
-				mv out.jpg data/ground_truth/"$sample"/
-			else
-				echo "$sample panorama failed" >> log.txt
-			fi
+			echo "$sample" panorama failed
 		fi
-	done
-fi
+	fi
+done
